7ab49bc20622ec4cfa6f004f6aae91d238898a6f
Kelsie Brown <38116231+kelsiebrown@users.noreply.github.com>
2019-05-23 11:51:55 -0700

    Add UI and functionality

---
+++ src/main/java/com/example/tasklist/List.java
@@ -0,0 +1,39 @@
+package com.example.tasklist;
+
+public class List {
+
+    private int id;
+    private String name;
+
+    public List() {}
+
+    public List(String name) {
+        this.name = name;
+    }
+
+    public List(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return name;   // used for add/edit spinner
+    }
+}
